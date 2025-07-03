31bd76600c47c562461854870608ca1d4d77af01
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-07-03 12:41:43 +0100

    continue in new project u1-w3-d4-jpql

--- src/main/java/ivancroce/Application.java
+++ src/main/java/ivancroce/Application.java
@@ -1,14 +1,10 @@
 package ivancroce;
 
 import ivancroce.dao.EventDAO;
-import ivancroce.entities.Event;
-import ivancroce.entities.EventType;
 import ivancroce.exceptions.NotFoundException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-
-import java.time.LocalDate;
 
 public class Application {
     // 1. STEP: create the emf to connect the DB
@@ -19,15 +15,15 @@ public class Application {
         EventDAO ed = new EventDAO(em); // instance of our DAO, passing em
 
         // creating a new event
-        Event birthday = new Event("Aldo Baglio's b-day", LocalDate.of(2025, 07, 01), "Big Party!", EventType.PRIVATE, 30);
-        Event concert = new Event("Aldo Baglio's Concert", LocalDate.of(2025, 10, 12), "Music concert", EventType.PUBLIC, 100);
+        // Event birthday = new Event("Aldo Baglio's b-day", LocalDate.of(2025, 07, 01), "Big Party!", EventType.PRIVATE, 30);
+        // Event concert = new Event("Aldo Baglio's Concert", LocalDate.of(2025, 10, 12), "Music concert", EventType.PUBLIC, 100);
 
-        ed.save(birthday); // to comment this one otherwise save method keeps creating the event in the DB
-        ed.save(concert);
+        // ed.save(birthday); // to comment this one otherwise save method keeps creating the event in the DB
+        // ed.save(concert);
 
         try {
-            Event aldoFromDB = ed.findById("519d2fc1-c1c5-4bab-9d8a-4e1737253f21");
-            System.out.println(aldoFromDB); // findById method
+            /*Event aldoFromDB = ed.findById("519d2fc1-c1c5-4bab-9d8a-4e1737253f21");
+            System.out.println(aldoFromDB);*/ // findById method
 
             // ed.findByIdAndDelete(3); to delete
 
--- src/main/java/ivancroce/entities/ParticipationState.java
+++ src/main/java/ivancroce/entities/ParticipationState.java
@@ -1,4 +1,5 @@
 package ivancroce.entities;
 
 public enum ParticipationState {
+    CONFIRMED, TO_BE_CONFIRMED
 }
